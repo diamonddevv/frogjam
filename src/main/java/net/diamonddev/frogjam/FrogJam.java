dde93fc2b80f40f6d0f9afd5dde5658e415a8a17
DiamondDev <fynndiamond@gmail.com>
2023-01-11 11:01:48 +0000

    1.19.3

--- src/main/java/net/diamonddev/frogjam/FrogJam.java
+++ src/main/java/net/diamonddev/frogjam/FrogJam.java
@@ -1,9 +1,10 @@
 package net.diamonddev.frogjam;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,14 +14,14 @@ public class FrogJam implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("FrogJam");
 
 	private static final Identifier frogjamSoundId = new Identifier(modid, "entity.frog.frogjam");
-	public static SoundEvent FROGJAM = new SoundEvent(frogjamSoundId);
+	public static SoundEvent FROGJAM = SoundEvent.of(frogjamSoundId);
 	public static double frogjamLengthInSeconds = 7.436;
 
 	@Override
 	public void onInitialize() {
 		long start = System.currentTimeMillis();
 		//
-		Registry.register(Registry.SOUND_EVENT, frogjamSoundId, FROGJAM); // Register Sound
+		Registry.register(Registries.SOUND_EVENT, frogjamSoundId, FROGJAM); // Register Sound
 		//
 		long initTime = System.currentTimeMillis() - start;
 		LOGGER.info("Mod " + modid + " initialized in " + initTime + " millisecond(s)!");
